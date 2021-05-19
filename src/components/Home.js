b03f2357bf7647a471bb124936d387b3f5e24368
Sally-Shum <sally20100529@gmail.com>
2021-05-19 23:42:20 +0800

    update Home page as one page control menu

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,31 +1,86 @@
+// eslint-disable-next-line
 import React, { useState, useEffect } from "react";
-
+// eslint-disable-next-line
 import UserService from "../services/user.service";
+// eslint-disable-next-line
+import { Dropdown, Option } from "./Dropdown";
+import { maxView, conferenceNames, images } from "./constant";
+import "./Home.scss";
+import { Formik } from 'formik';
 
 const Home = () => {
-  const [content, setContent] = useState("");
 
-  useEffect(() => {
-    UserService.getPublicContent().then(
-      (response) => {
-        setContent(response.data);
-      },
-      (error) => {
-        const _content =
-          (error.response && error.response.data) ||
-          error.message ||
-          error.toString();
+  // const [conference, setConference] = useState("");
+  // const [style, setStyle] = useState("");
+  // const [content, setContent] = useState("");
+  const [name, setName] = useState("");
 
-        setContent(_content);
-      }
-    );
-  }, []);
+  // useEffect(() => {
+  //   UserService.getPublicContent().then(
+  //     (response) => {
+  //       setContent(response.data);
+  //     },
+  //     (error) => {
+  //       const _content =
+  //         (error.response && error.response.data) ||
+  //         error.message ||
+  //         error.toString();
+
+  //       setContent(_content);
+  //     }
+  //   );
+  // }, []);
+const choicesOfViews =  Array(maxView).fill(0);
 
   return (
     <div className="container">
-      <header className="jumbotron">
-        <h3>{content}</h3>
-      </header>
+      <Dropdown
+        formLabel="Conference Name"
+        buttonText="Send form"
+        action="/"
+      >
+        {conferenceNames.map((cname) =>
+          <Option value={cname} />
+        )}
+      </Dropdown>
+
+      <Dropdown
+        formLabel="Number of View(s):"
+        buttonText="Send form"
+        action="/"
+      >
+        <Option selected value="Favour" />
+        {choicesOfViews.map((value, index) =>
+          <Option value={index+1} />
+        )}
+      </Dropdown>
+
+      { images.map(({id, src, title, description}) =>
+      <button>
+        <img class = "imgBtn" key={id} src={src} title={title} alt={description} />
+      </button>
+      )}
+
+      <Dropdown
+        formLabel="Save As:"
+        buttonText="Save"
+        action="/"
+      >
+        <Option selected value={1} />
+        <Option value="1" />
+        <Option value="2" />
+        <Option value="3" />
+        <Option value="4" />
+        <Option value="5" />
+        <Option value="6" />
+      </Dropdown>
+      <input
+             type="text"
+             name="styleName"
+            //  onBlur={handleBlur}
+            //  value={setName}
+           />
+
     </div>
   );
 };
---
+++ src/components/constant.js
@@ -0,0 +1,10 @@
+export const conferenceNames = ["conferenceA", "conferenceB", "conferenceC"];
+export const maxView = 6;
+
+export const images = [
+    {id:1, src: "../img/1/1.jpg", title:'1_1', description: '1'},
+    {id:2, src: "../img/2/1.jpg", title:'2_1', description: '2'},
+    {id:3, src: "../img/2/2.jpg", title:'2_2', description: '2'},
+    {id:4, src: "../img/2/3.jpg", title:'2_3', description: '2'},
+    {id:5, src: "../img/3/1.jpg", title:'3_1', description: '3'}
+];
